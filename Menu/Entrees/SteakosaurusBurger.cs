3f8b7f35fa0528b315b242226f98fbd056a1131d
jtschenk <jtschenk@F1092-03-2091>
2019-09-09 17:02:00 -0500

    First Commit

---
+++ Menu/Entrees/SteakosaurusBurger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Menu.Entrees
+{
+    public class SteakosaurusBurger
+    {
+        private bool wholewheatbun = true;
+        private bool pickle = true;
+        private bool ketchup = true;
+        private bool mustard = true;
+
+        public double Price { get; set; }
+
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+
+        }
+
+    }
+}
