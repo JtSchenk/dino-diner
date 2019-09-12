c872113ca3b2b2b018095a8ce352e62da74da4d9
Jacob Schenkelberg <jtschenk@ksu.edu>
2019-09-12 17:37:07 -0500

    Second Commit. Halfway there!

--- Menu/Entrees/Brontowurst.cs
+++ Menu/Entrees/Brontowurst.cs
@@ -6,6 +6,48 @@ namespace Menu
 {
     public class Brontowurst
     {
+        //private bool brautwurst = true; will always be true, no need for this code
 
+        private bool wholewheatbun = true;
+        private bool peppers = true;
+        private bool onions = true;
+
+
+        public double Price { get; set; }
+
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Brautwurst" };
+                if (wholewheatbun) ingredients.Add("Whole-Wheat Bun");
+                if (peppers) ingredients.Add("Peppers");
+                if (onions) ingredients.Add("Onions");
+                return ingredients;
+            }
+        }
+
+        public Brontowurst()
+        {
+            this.Price = 5.36;
+            this.Calories = 498;
+        }
+
+        public void HoldWholeWheatBun()
+        {
+            this.wholewheatbun = false;
+        }
+
+        public void HoldPeppers()
+        {
+            this.peppers = false;
+        }
+
+        public void HoldOnions()
+        {
+            this.onions = false;
+        }
     }
 }
--- Menu/Entrees/SteakosaurusBurger.cs
+++ Menu/Entrees/SteakosaurusBurger.cs
@@ -14,10 +14,45 @@ namespace Menu.Entrees
         public double Price { get; set; }
 
         public uint Calories { get; set; }
-
-        public List<string> //Ingredients
+        
+        public List<string> Ingredients
         {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Steakburger Pattie" }; //Ingredients
+                if (wholewheatbun) ingredients.Add("Whole Wheat Bun");
+                if (pickle) ingredients.Add("Pickle");
+                if (ketchup) ingredients.Add("Ketchup");
+                if (mustard) ingredients.Add("Mustard");
+                return ingredients;
+            }
 
+        }
+
+        public SteakosaurusBurger()
+        {
+            this.Price = 5.15;
+            this.Calories = 621;
+        }
+
+        public void HoldBun()
+        {
+            this.wholewheatbun = false;
+        }
+
+        public void HoldPickle()
+        {
+            this.pickle = false;
+        }
+                
+        public void HoldKetchup()
+        {
+            this.ketchup = false;
+        }
+
+        public void HoldMustard()
+        {
+            this.mustard = false;
         }
 
     }
