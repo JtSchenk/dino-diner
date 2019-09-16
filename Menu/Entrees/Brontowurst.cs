57599ec76c2c381b8f7194e99fa72e8198196c05
jtschenk <jtschenk@F1092-03-2091>
2019-09-16 15:38:02 -0500

    Just did some practicing with commenting

--- Menu/Entrees/Brontowurst.cs
+++ Menu/Entrees/Brontowurst.cs
@@ -6,17 +6,30 @@ namespace DinoDiner.Menu.Entrees
 {
     public class Brontowurst
     {
-        //private bool brautwurst = true; will always be true, no need for this code
-
+        
+        // private field that intiliates the bun to be true. Later has a method that can change it to false.
         private bool wholewheatbun = true;
+
+        // private field that intiliates the peppers to be true. Later has a method that can change it to false.
         private bool peppers = true;
+
+        // private field that intiliates the onions to be true. Later has a method that can change it to false.
         private bool onions = true;
 
-
+        /// <summary>
+        /// This public field gets and sets the Price.
+        /// </summary>
         public double Price { get; set; }
 
+        /// <summary>
+        /// This public field gets and sets the Calories.
+        /// </summary>
         public uint Calories { get; set; }
 
+        /// <summary>
+        /// This method adds the string of the item if it's boolean value is set to "true".
+        /// IT then returns the full list of ingredients.
+        /// </summary>
         public List<string> Ingredients
         {
             get
