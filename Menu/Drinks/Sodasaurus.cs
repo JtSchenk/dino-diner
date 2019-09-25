ced835d3b01d6a58b75974566f9a558dcf4f4503
jtschenk <jtschenk@F1092-03-2091>
2019-09-25 16:28:41 -0500

    Test cases made for Milestone 3 (lecture)

---
+++ Menu/Drinks/Sodasaurus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    public class Sodasaurus
+    {
+        private SodasaurusFlavor flavor;
+        public SodasaurusFlavor Flavor
+        {
+            get { return flavor; }
+            set { flavor = value; }
+        }
+
+        public double Price { get; set; } = 1.5;
+
+        private Size size;
+        public Size Size
+        {
+            get { return size; }
+            set
+            {
+                size = value;
+                if (size == Size.Medium)
+                {
+                    Price = 2.00;
+                }
+            }
+        }
+
+        public bool Ice { get; set; } = true;
+    }
+}
---
+++ MenuTest/Drinks/SodasaurusTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.Drinks;
+using Xunit;
+using DinoDiner.Menu;
+
+namespace MenuTest.Drinks
+{
+    /*
+     * The ability to set each possible flavor
+       The correct default price, calories, ice, and size
+       The correct price and calories after changing to small, medium, and large sizes.
+       That invoking HoldIce() results in the Ice property being false.
+     */
+    public class SodasaurusTest
+    {
+        // The ability to set each possible flavor
+        [Fact]
+        public void ShouldBeAbleToSetFlavorToCherry()
+        {
+            Sodasaurus soda = new Sodasaurus();
+            soda.Flavor = SodasaurusFlavor.Cherry;
+            Assert.Equal<SodasaurusFlavor>(SodasaurusFlavor.Cherry, soda.Flavor);
+        }
+
+        // The correct default price, calories, ice, and size
+        [Fact]
+        public void ShouldHaveCorrectDefaultPrice()
+        {
+            Sodasaurus soda = new Sodasaurus();
+            Assert.Equal<double>(1.50, soda.Price);
+        }
+
+        // The correct price and calories after changing to small, medium, and large
+        [Fact]
+        public void ShouldHaveCorrectPriceAfterSettingSmall()
+        {
+            Sodasaurus soda = new Sodasaurus();
+            soda.Size = Size.Medium;
+            soda.Size = Size.Small;
+            Assert.Equal<double>(1.50, soda.Price);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectPriceAfterSettingMedium()
+        {
+            Sodasaurus soda = new Sodasaurus();
+            soda.Size = Size.Medium;
+            Assert.Equal<double>(2.00, soda.Price);
+        }
+
+        [Fact]
+        public void ShouldHaveDefaultIce()
+        {
+            Sodasaurus soda = new Sodasaurus();
+            Assert.True(soda.Ice);
+        }
+    }
+}
