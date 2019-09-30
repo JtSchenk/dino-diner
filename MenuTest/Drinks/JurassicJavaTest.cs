9826fd91d327f191856ee0b4113d3e28d74c1cad
jtschenk <jtschenk@F1093-25-2284>
2019-09-30 14:23:08 -0500

    JurassicJavaTest cases added

---
+++ MenuTest/Drinks/JurassicJavaTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.Drinks;
+using Xunit;
+using DinoDiner.Menu;
+
+namespace MenuTest.Drinks
+{
+    public class JurassicJavaTest
+    {
+        [Fact]
+        public void ShouldHaveCorrectDefaultPrice()
+        {
+            //JurassicJava java = new JurassicJava();
+            //Assert.Equal<double>(1.50, java.Price);
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectDefaultCalories()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectDefaultIce()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectDefaultSize()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectDefaultLemon()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectPriceAfterSettingSmall()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectPriceAfterSettingMedium()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectPriceAfterSettingLarge()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesAfterSettingSmall()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesAfterSettingMedium()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesAfterSettingLarge()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldAddIce()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldLeaveSpaceForCream()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectIngredients()
+        {
+
+        }
+    }
+}
