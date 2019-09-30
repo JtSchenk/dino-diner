2831e2f57fc31e70998112fee0f66a72891a382d
jtschenk <jtschenk@F1093-25-2284>
2019-09-30 14:26:13 -0500

    Every test case added

---
+++ MenuTest/Drinks/WaterTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.Drinks;
+using Xunit;
+using DinoDiner.Menu;
+
+namespace MenuTest.Drinks
+{
+    public class WaterTest
+    {
+        [Fact]
+        public void ShouldHaveDefaultPrice()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveDefaultCalories()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveDefaultSize()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveDefaultLemon()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectPriceAfterSettingSmall()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectPriceAfterSettingMedium()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectPriceAfterSettingLarge()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesAfterSettingSmall()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesAfterSettingMedium()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHaveCorrectCaloriesAfterSettingLarge()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldHoldIce()
+        {
+
+        }
+
+        [Fact]
+        public void ShouldAddLemon()
+        {
+
+        }
+
+        public void ShouldHaveCorrectIngredients()
+        {
+
+        }
+    }
+}
